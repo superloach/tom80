e4137f52287eec758835318062b5aff3efa86ea9
Terra Brown <superloach42@gmail.com>
2020-01-19 09:37:29 -0500

    control test directionals

--- prgm/test/test.asm
+++ prgm/test/test.asm
@@ -5,16 +5,57 @@
 loop:
 	IN	A,	(1)
 	AND	10000000B
-	JR	NZ,	set
-	JP	unset
-
-set:
-	v_poke	0,	0,	c_red
+	JR	NZ,	upset
+	JP	upunset
+	updone:
+	IN	A,	(1)
+	AND	01000000B
+	JR	NZ,	downset
+	JP	downunset
+	downdone:
+	IN	A,	(1)
+	AND	00100000B
+	JR	NZ,	leftset
+	JP	leftunset
+	leftdone:
+	IN	A,	(1)
+	AND	00010000B
+	JR	NZ,	rightset
+	JP	rightunset
+	rightdone:
 	JP	loop
 
-unset:
-	v_poke	0,	0,	c_black
-	JP	loop
+upset:
+	v_poke	2,	1,	c_red
+	JP	updone
+
+upunset:
+	v_poke	2,	1,	c_black
+	JP	updone
+
+downset:
+	v_poke	2,	2,	c_red
+	JP	downdone
+
+downunset:
+	v_poke	2,	2,	c_black
+	JP	downdone
+
+leftset:
+	v_poke	1,	2,	c_red
+	JP	leftdone
+
+leftunset:
+	v_poke	1,	2,	c_black
+	JP	leftdone
+
+rightset:
+	v_poke	3,	2,	c_red
+	JP	rightdone
+
+rightunset:
+	v_poke	3,	2,	c_black
+	JP	rightdone
 
 v_poke:	.macro x, y, c
 	LD	DE,	64 * (y) + (x)
